10a26c86422934c1ab66e358ec90458b7bec8f06
sarahknow <sarahknow@outlook.com>
2014-10-07 11:12:46 -0400

    Added the last 4 ships

--- js/main.js
+++ js/main.js
@@ -6,11 +6,8 @@ var $dipper = $('.dipper')
 var $shuttleSection = $('.shuttle-section')
 var $shuttle = $('.shuttle')
 var $shipSection = $('.ship-section')
-var $ship = $('.ship')
-var $ship1 = $('.ship-1')
-var $ship2 = $('.ship-2')
-var $ship3 = $('.ship-3')
-var $ship4 = $('.ship-4')
+var $ships = $('.ships img')
+
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -28,15 +25,6 @@ $shuttleSection.waypoint(function () {
 }, {offset: '50%'});
 
 $shipSection.waypoint(function () {
-	$ship.addClass('js-ship-fade');
+	$ships.addClass('js-ships-animate');
 }, {offset: '50%'});
 
-$shipSection.waypoint(function () {
-	$ship1.addClass('js-ship-1-3');
-	$ship3.addClass('js-ship-1-3');
-}, {offset: '50%'});
-
-$shipSection.waypoint(function () {
-	$ship2.addClass('js-ship-2-4');
-	$ship4.addClass('js-ship-2-4');
-}, {offset: '50%'});
